2c9d5f55672f0b3e73629c8da623b33ccff714f6
Àlex Llaó <allaog@hotmail.com>
2014-06-27 16:46:34 +0200

    First update

---
+++ NSString+Levenshtein.h
@@ -0,0 +1,14 @@
+//
+// NSString+Levenshtein.h
+//
+// Modified by Àlex Llaó on 06/27/14.
+// <http://mabblog.com>
+//
+// Created by Àlex Llaó on Fri Jul 27 2014.
+// alexllao@me.com
+
+#import <Foundation/Foundation.h>
+
+@interface NSString (Levenshtein)
+	-(int)LevenshteinDistance:(NSString*)s1 s2:(NSString*)s2;
+@end
---
+++ NSString+Levenshtein.m
@@ -0,0 +1,16 @@
+//
+// NSString+Levenshtein.h
+//
+// Modified by Àlex Llaó on 06/27/14.
+// <http://mabblog.com>
+//
+// Created by Àlex Llaó on Fri Jul 27 2014.
+// alexllao@me.com
+
+#import "NSString+Levenshtein.h"
+
+@implementation NSString (Levenshtein)
+	-(int)LevenshteinDistance:(NSString*)s1 s2:(NSString*)s2 {
+		
+	}
+@end
