e2e395d4fcc47db2220913323686e5e573806a04
Àlex Llaó <allaog@hotmail.com>
2018-01-23 12:00:10 +0100

    Update NSString+Levenshtein.m

--- NSString+Levenshtein/NSString+Levenshtein.m
+++ NSString+Levenshtein/NSString+Levenshtein.m
@@ -1,9 +1,9 @@
 //
 // NSString+Levenshtein.h
 //
-// Modified by Àlex Llaó on 06/27/14.
+// Modified by Ã€lex LlaÃ³ on 06/27/14.
 //
-// Created by Àlex Llaó on Fri Jul 27 2014.
+// Created by Ã€lex LlaÃ³ on Fri Jul 27 2014.
 // alexllao (at) me.com
 // Based on the implementation of the algorithms 
 // proposed in Wikipedia "http://en.wikipedia.org/wiki/Levenshtein_distance".
@@ -41,54 +41,4 @@
     // Return LevenshteinDistance
     return [[d objectAtRow:m column:n] intValue];
 }
-
-/*- (int)DamerauLevenshteinDistance:(NSString*)s2 threshold:(int)threshold {
-    if ([self isEqualToString:s2]) return 0;
-    if ([self isEmpty]) return (int)[s2 length];
-    if ([s2 isEmpty]) return  0;
-    
-    if ([s2 rangeOfString:self options:NSCaseInsensitiveSearch].location == NSNotFound) { return (int)s2.length - (int)self.length; }
-    if ([self rangeOfString:s2 options:NSCaseInsensitiveSearch].location == NSNotFound) { return 0; }
-    
-    int length1 = (int)self.length;
-    int length2 = (int)s2.length;
-    
-    CRL2DArray * d = [[CRL2DArray alloc] initWithRows:2 columns:length2 + 1];
-    for (int i = 0; i <= length1 - 1; i++) { [d insertObject:[NSNumber numberWithInt:0] atRow:i column:0]; }
-    for (int j = 0; j <= length2; j++) { [d insertObject:[NSNumber numberWithInt:j] atRow:0 column:j]; }
-    
-    for (int i = 1; i<= length1 - 1; i++) {
-        int im1 = i - 1;
-        int im2 = i - 2;
-        int minDistance = threshold;
-        
-        for (int j = 1; j<=length2; j++) {
-            int jm1 = j - 1;
-            int jm2 = j - 2;
-            int cost = ([self characterAtIndex:im1] == [s2 characterAtIndex:jm1] ? 0 : 1);
-            
-            int del = [[d objectAtRow:im1 column:j] integerValue] + 1;
-            int ins = [[d objectAtRow:i column:jm1] integerValue] + 1;
-            int sub = [[d objectAtRow:im1 column:jm1] integerValue] + cost;
-            
-            /*
-             d[i, j] = del <= ins && del <= sub ? del : ins <= sub ? ins : sub;
-             
-             if (i > 1 && j > 1 && string1[im1] == string2[jm2] && string1[im2] == string2[jm1])
-             d[i, j] = Math.Min(d[i, j], d[im2, jm2] + cost);
-             
-             if (d[i, j] < minDistance)
-             minDistance = d[i, j];
-             */
-        }
-        
-        if (minDistance > threshold) return MAX_INPUT;
-    }
-    
-    return 0;
-    // return [[d insertObject:0 atRow:2 column:length2 + 1] intValue] > threshold ? MAX_INPUT : [[d objectAtRow:2 column: length2] integerValue];
-}*/
-/*- (BOOL)isEmpty {
-    return ([[self stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]]length] == 0);
-}*/
 @end
